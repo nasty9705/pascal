e171f2d399070f4e2b6faaf170e870674f90837f
Totktonada <alextux@bk.ru>
2011-02-14 03:22:30 +0300

    trees.pas: Remove strange code, fix formatting.

--- trees.pas
+++ trees.pas
@@ -7,116 +7,53 @@ type tree_type = integer;
               left:tree;
               right:tree;
             end;
+var t1:tree;
 
-     list = ^list_item;
-     list_item = record
-              elem:tree;
-              next:list;
-            end;
-var t1,empty:tree;
-    l1:list;
-
-procedure init(t1:tree);
+procedure init(var t1:tree);
 begin
-  new(empty);
-  t1:=empty;
+  t1:=nil;
 end;
 
 function isEmpty(t:tree):boolean;
 begin
-  if (t<>nil) then
-    isEmpty:=(t=empty)
-  else isEmpty:=true;
+  isEmpty:=(t=nil);
+end;
+
+function newItem(e:tree_type):tree;
+var
+  newItem_var:tree;
+begin
+  new(newItem_var);
+  newItem_var^.elem := e;
+  newItem_var^.left := nil;
+  newItem_var^.right := nil;
+  newItem := newItem_var;
 end;
 
 procedure add(var t:tree; e:tree_type);
-var toLeft:boolean;
-    newItem,next_tree:tree;
 begin
   if (not isEmpty(t)) then begin
-    if (e<t^.elem) then begin
-      toLeft:=true;
-      next_tree:=t^.left
-    end else begin
-      toLeft:=false;
-      next_tree:=t^.right;
-    end;
-  if (not isEmpty(next_tree)) then
-    add(next_tree, e)
-  else begin
-         new(newItem);
-         newItem^.elem:=e;
-         if (toLeft) then
-           t^.left:=newItem
-         else t^.right:=newItem;
-  end end else begin
-             new(newItem);
-             newItem^.elem:=e;
-             t:=newItem;
-           end;
+    if (e < t^.elem) then
+      add(t^.left, e)
+    else
+      add(t^.right, e);
+  end else begin
+    t := newItem(e);
+  end;
 end;
 
 procedure show(t:tree);
-var l,r:tree;
-    b:boolean;
-    p,tmp:list;
 begin
-  if (not isEmpty(t)) then
-     new(l1);
-     l1^.elem:=t;
-     b:=true;
-     while b do begin
-        b:=false;
-        l:=l1^.elem^.left;
-        r:=l1^.elem^.right;
-        { left }
-        if (not isEmpty(l)) then begin
-           write(l^.elem);
-           tmp:=l1;
-           l1:=l1^.next;
-           tmp:=l1;
-           while(tmp^.next<>nil) do
-              tmp:=tmp^.next;
-           new(p);
-           p^.elem:=l;
-           p^.next:=nil;
-           tmp^.next:=p;
-           dispose(tmp);
-           b:=true;
-          end;
-        { right }
-        if (not isEmpty(r)) then begin
-           write(r^.elem);
-           tmp:=l1;
-           l1:=l1^.next;
-           tmp:=l1;
-           while(tmp^.next<>nil) do
-              tmp:=tmp^.next;
-           new(p);
-           p^.elem:=r;
-           p^.next:=nil;
-           tmp^.next:=p;
-           dispose(t);
-           b:=true;
-          end;
-     end;
-
-
-
-{
-  writeln('[',level,']',':',t^.elem);
+  writeln('(',t^.elem,')');
   if (not isEmpty(t^.left)) then begin
     write('[L]:');
-    level := level+1;
     show(t^.left);
-    level := level-1;
   end;
   if (not isEmpty(t^.right)) then begin
     write('[R]:');
-    level := level+1;
     show(t^.right);
-    level := level-1;
-  end;}
+  end;
+  writeln('[UP]');
 end;
 
 begin
